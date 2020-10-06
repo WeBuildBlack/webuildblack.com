59775d00e0da1d5cb97f5b5fb32c0b535f6b20aa
Chloe Rice <chloe.rice@shopify.com>
2020-10-06 11:22:13 -0400

    [Nav] Fix Bounty Board link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,104 +1,104 @@
-import React, { useCallback, useState, useRef, useEffect } from 'react'
-import { Link, navigate } from 'gatsby'
-import classNames from 'classnames'
+import React, { useCallback, useState, useRef, useEffect } from "react";
+import { Link, navigate } from "gatsby";
+import classNames from "classnames";
 
-import wbbWordmark from '../../assets/images/wbb-wordmark-white-no-bg.svg'
+import wbbWordmark from "../../assets/images/wbb-wordmark-white-no-bg.svg";
 
-import styles from './Nav.module.scss'
+import styles from "./Nav.module.scss";
 
 export default function Nav() {
-  const aboutRef = useRef(null)
-  const getInvolvedRef = useRef(null)
-  const makeAnImpactRef = useRef(null)
-  const donateRef = useRef(null)
-  const mavensRef = useRef(null)
-  const meetupsRef = useRef(null)
-  const bountyBoardRef = useRef(null)
-  const slackRef = useRef(null)
-  const hireRef = useRef(null)
-  const partnerRef = useRef(null)
-  const volunteerRef = useRef(null)
-  const navItemRefs = [aboutRef, getInvolvedRef, makeAnImpactRef, donateRef]
-  const getInvolvedRefs = [mavensRef, meetupsRef, bountyBoardRef, slackRef]
-  const makeAnImpactRefs = [hireRef, partnerRef, volunteerRef]
-  const [activeMenuItemIndex, setActiveMenuItemIndex] = useState(null)
-  const [activeSubMenuItemIndex, setActiveSubMenuItemIndex] = useState(null)
+  const aboutRef = useRef(null);
+  const getInvolvedRef = useRef(null);
+  const makeAnImpactRef = useRef(null);
+  const donateRef = useRef(null);
+  const mavensRef = useRef(null);
+  const meetupsRef = useRef(null);
+  const bountyBoardRef = useRef(null);
+  const slackRef = useRef(null);
+  const hireRef = useRef(null);
+  const partnerRef = useRef(null);
+  const volunteerRef = useRef(null);
+  const navItemRefs = [aboutRef, getInvolvedRef, makeAnImpactRef, donateRef];
+  const getInvolvedRefs = [mavensRef, meetupsRef, bountyBoardRef, slackRef];
+  const makeAnImpactRefs = [hireRef, partnerRef, volunteerRef];
+  const [activeMenuItemIndex, setActiveMenuItemIndex] = useState(null);
+  const [activeSubMenuItemIndex, setActiveSubMenuItemIndex] = useState(null);
 
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const toggleMobileMenu = useCallback(() => {
-    setMobileMenuOpen(mobileMenuOpen => !mobileMenuOpen)
-  })
+    setMobileMenuOpen((mobileMenuOpen) => !mobileMenuOpen);
+  });
 
-  const [dropdownOpen, setDropdownOpen] = useState(false)
-  const [activeDropdown, setActiveDropdown] = useState('')
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState("");
 
   const handleFocusMenuItem = useCallback(
-    event => {
-      const index = event.target.getAttribute('data-index')
-      setActiveMenuItemIndex(Number(index))
+    (event) => {
+      const index = event.target.getAttribute("data-index");
+      setActiveMenuItemIndex(Number(index));
     },
     [setActiveMenuItemIndex, activeMenuItemIndex]
-  )
+  );
 
   const getInvolvedClassName = classNames(
     styles.DropdownItem,
-    activeDropdown === 'Get involved' && dropdownOpen && styles.active
-  )
+    activeDropdown === "Get involved" && dropdownOpen && styles.active
+  );
 
   const makeAnImpactClassName = classNames(
     styles.DropdownItem,
-    activeDropdown === 'Make an impact' && dropdownOpen && styles.active
-  )
+    activeDropdown === "Make an impact" && dropdownOpen && styles.active
+  );
 
-  const menuClassName = classNames(styles.Menu, mobileMenuOpen && styles.open)
+  const menuClassName = classNames(styles.Menu, mobileMenuOpen && styles.open);
   const mobileMenuButtonClassName = classNames(
     styles.MobileMenuToggle,
     styles.NavButton,
-    'button'
-  )
+    "button"
+  );
   const donateButtonClassName = classNames(
     styles.NavButton,
     styles.DonateButton,
-    'button'
-  )
+    "button"
+  );
 
   const closeButtonClassName = classNames(
     styles.NavButton,
     styles.CloseButton,
-    'button'
-  )
+    "button"
+  );
 
   const isDropdown = useCallback(
-    name => name === 'Get involved' || name === 'Make an impact',
+    (name) => name === "Get involved" || name === "Make an impact",
     []
-  )
+  );
 
   const getIndexToFocus = useCallback(
-    (direction, lastIndex) => (direction === 'up' ? lastIndex : 0),
+    (direction, lastIndex) => (direction === "up" ? lastIndex : 0),
     []
-  )
+  );
 
   const focusNextSubMenuItem = useCallback(
     (name, direction) => {
       const refList =
-        name === 'Get involved' ? getInvolvedRefs : makeAnImpactRefs
+        name === "Get involved" ? getInvolvedRefs : makeAnImpactRefs;
 
-      const lastIndex = refList.length - 1
-      let nextItemIndex = activeSubMenuItemIndex
+      const lastIndex = refList.length - 1;
+      let nextItemIndex = activeSubMenuItemIndex;
 
       if (nextItemIndex === null) {
-        nextItemIndex = getIndexToFocus(direction, lastIndex)
-      } else if (direction === 'up') {
-        nextItemIndex = nextItemIndex === 0 ? lastIndex : nextItemIndex - 1
-      } else if (direction === 'down') {
-        nextItemIndex = nextItemIndex === lastIndex ? 0 : nextItemIndex + 1
+        nextItemIndex = getIndexToFocus(direction, lastIndex);
+      } else if (direction === "up") {
+        nextItemIndex = nextItemIndex === 0 ? lastIndex : nextItemIndex - 1;
+      } else if (direction === "down") {
+        nextItemIndex = nextItemIndex === lastIndex ? 0 : nextItemIndex + 1;
       }
 
-      const nextItem = refList[nextItemIndex]
-      setActiveSubMenuItemIndex(nextItemIndex)
+      const nextItem = refList[nextItemIndex];
+      setActiveSubMenuItemIndex(nextItemIndex);
 
       if (nextItem && nextItem.current) {
-        nextItem.current.focus()
+        nextItem.current.focus();
       }
     },
     [
@@ -107,157 +107,157 @@ export default function Nav() {
       activeDropdown,
       setActiveSubMenuItemIndex,
     ]
-  )
+  );
 
   const closeDropdown = useCallback(() => {
-    setActiveDropdown('')
-    setActiveSubMenuItemIndex(null)
-    setDropdownOpen(false)
-  })
-  const openDropdown = useCallback(name => {
-    setActiveDropdown(name)
-    setDropdownOpen(true)
-  })
+    setActiveDropdown("");
+    setActiveSubMenuItemIndex(null);
+    setDropdownOpen(false);
+  });
+  const openDropdown = useCallback((name) => {
+    setActiveDropdown(name);
+    setDropdownOpen(true);
+  });
 
   const toggleDropdownMenu = useCallback(
-    name => {
+    (name) => {
       if (activeDropdown === name) {
-        return closeDropdown()
+        return closeDropdown();
       }
       if (activeDropdown !== name) {
-        return openDropdown(name)
+        return openDropdown(name);
       }
     },
     [dropdownOpen, activeDropdown]
-  )
+  );
 
-  const handleClickDropdown = useCallback(event => {
-    const name = event.target.getAttribute('data-navItem')
-    toggleDropdownMenu(name)
-  })
+  const handleClickDropdown = useCallback((event) => {
+    const name = event.target.getAttribute("data-navItem");
+    toggleDropdownMenu(name);
+  });
 
   useEffect(() => {
     if (dropdownOpen && activeSubMenuItemIndex) {
       const refList =
-        activeDropdown === 'Get involved' ? getInvolvedRefs : makeAnImpactRefs
+        activeDropdown === "Get involved" ? getInvolvedRefs : makeAnImpactRefs;
 
-      const menuItem = refList[activeSubMenuItemIndex]
+      const menuItem = refList[activeSubMenuItemIndex];
 
-      if (menuItem && menuItem.current) menuItem.current.focus()
+      if (menuItem && menuItem.current) menuItem.current.focus();
     }
-  }, [activeSubMenuItemIndex, activeDropdown, dropdownOpen])
+  }, [activeSubMenuItemIndex, activeDropdown, dropdownOpen]);
 
   const handleDropdownKeyPress = useCallback(
-    event => {
-      const navItem = event.target.getAttribute('data-navItem')
+    (event) => {
+      const navItem = event.target.getAttribute("data-navItem");
 
       switch (event.key) {
-        case 'Enter':
-        case 'Space':
-          return
+        case "Enter":
+        case "Space":
+          return;
 
-        case 'ArrowDown':
-          event.stopPropagation()
-          event.preventDefault()
+        case "ArrowDown":
+          event.stopPropagation();
+          event.preventDefault();
           if (!dropdownOpen) {
-            openDropdown(navItem)
+            openDropdown(navItem);
           }
 
-          focusNextSubMenuItem(navItem, 'down')
-          break
+          focusNextSubMenuItem(navItem, "down");
+          break;
 
-        case 'ArrowUp':
-          event.stopPropagation()
-          event.preventDefault()
+        case "ArrowUp":
+          event.stopPropagation();
+          event.preventDefault();
           if (!dropdownOpen) {
-            openDropdown(navItem)
+            openDropdown(navItem);
           }
 
-          focusNextSubMenuItem(navItem, 'up')
-          break
+          focusNextSubMenuItem(navItem, "up");
+          break;
 
         default:
-          break
+          break;
       }
     },
     [dropdownOpen, activeSubMenuItemIndex, activeDropdown]
-  )
+  );
 
   const focusNextMenuItem = useCallback(
-    direction => {
-      let nextItemIndex = activeMenuItemIndex
+    (direction) => {
+      let nextItemIndex = activeMenuItemIndex;
 
-      if (direction === 'left') {
+      if (direction === "left") {
         nextItemIndex =
           activeMenuItemIndex === 0
             ? navItemRefs.length - 1
-            : activeMenuItemIndex - 1
-      } else if (direction === 'right') {
+            : activeMenuItemIndex - 1;
+      } else if (direction === "right") {
         nextItemIndex =
           activeMenuItemIndex === navItemRefs.length - 1
             ? 0
-            : activeMenuItemIndex + 1
+            : activeMenuItemIndex + 1;
       }
 
-      const nextItem = navItemRefs[nextItemIndex]
-      const nextItemName = nextItem.current.getAttribute('data-navItem')
-      setActiveMenuItemIndex(nextItemIndex)
+      const nextItem = navItemRefs[nextItemIndex];
+      const nextItemName = nextItem.current.getAttribute("data-navItem");
+      setActiveMenuItemIndex(nextItemIndex);
 
       if (nextItem && nextItem.current) {
         if (dropdownOpen && !isDropdown(nextItemName)) {
-          closeDropdown()
+          closeDropdown();
         } else if (dropdownOpen && isDropdown(nextItemName)) {
-          closeDropdown()
-          openDropdown(nextItemName)
+          closeDropdown();
+          openDropdown(nextItemName);
         }
 
-        nextItem.current.focus()
+        nextItem.current.focus();
       }
     },
     [activeMenuItemIndex, dropdownOpen]
-  )
+  );
 
-  const handleMenuKeyPress = useCallback(event => {
-    const navItem = event.target.getAttribute('data-navItem')
+  const handleMenuKeyPress = useCallback((event) => {
+    const navItem = event.target.getAttribute("data-navItem");
 
     switch (event.key) {
-      case 'Tab':
+      case "Tab":
         // Because only the first nav item, 'About' in this case, has a tabindex, we know if we didn't tab to 'About' we've tabbed outside of the menu and should close any open dropdown.
-        if (navItem !== 'About') return closeDropdown()
-        return
+        if (navItem !== "About") return closeDropdown();
+        return;
 
-      case 'Escape':
-        return closeDropdown()
+      case "Escape":
+        return closeDropdown();
 
-      case 'ArrowLeft':
-        event.preventDefault()
-        return focusNextMenuItem('left')
+      case "ArrowLeft":
+        event.preventDefault();
+        return focusNextMenuItem("left");
 
-      case 'ArrowRight':
-        event.preventDefault()
-        return focusNextMenuItem('right')
+      case "ArrowRight":
+        event.preventDefault();
+        return focusNextMenuItem("right");
 
       default:
-        break
+        break;
     }
-  })
+  });
 
-  const handleNavigate = useCallback(event => {
-    const url = event.target.getAttribute('data-href')
-    navigate(url)
-  })
+  const handleNavigate = useCallback((event) => {
+    const url = event.target.getAttribute("data-href");
+    navigate(url);
+  });
 
   const menuOpenTrigger = !mobileMenuOpen ? (
     <button className={mobileMenuButtonClassName} onClick={toggleMobileMenu}>
       Menu
     </button>
-  ) : null
+  ) : null;
 
   const menuCloseTrigger = mobileMenuOpen ? (
     <button className={closeButtonClassName} onClick={toggleMobileMenu}>
       <span className="fa fa-2x fa-close" />
     </button>
-  ) : null
+  ) : null;
 
   const menuMarkup = (
     <ul
@@ -297,7 +297,7 @@ export default function Nav() {
             data-index={1}
             className={styles.DropdownButton}
             aria-controls="get-involved-dropdown"
-            aria-expanded={activeDropdown === 'Get involved' && dropdownOpen}
+            aria-expanded={activeDropdown === "Get involved" && dropdownOpen}
             onFocus={handleFocusMenuItem}
             onClick={handleClickDropdown}
             ref={getInvolvedRef}
@@ -347,7 +347,6 @@ export default function Nav() {
                 ref={bountyBoardRef}
                 data-href="/get-involved/programs/bounty-board"
                 href="/get-involved/programs/bounty-board"
-                onClick={handleNavigate}
                 target="_blank"
                 rel="noreferrer"
               >
@@ -366,7 +365,6 @@ export default function Nav() {
                 data-index={3}
                 ref={slackRef}
                 tabIndex={-1}
-                onClick={handleNavigate}
               >
                 Slack
               </a>
@@ -386,7 +384,7 @@ export default function Nav() {
             data-index={2}
             className={styles.DropdownButton}
             aria-controls="make-an-impact-dropdown"
-            aria-expanded={activeDropdown === 'Make an impact' && dropdownOpen}
+            aria-expanded={activeDropdown === "Make an impact" && dropdownOpen}
             onFocus={handleFocusMenuItem}
             onClick={handleClickDropdown}
             ref={makeAnImpactRef}
@@ -482,7 +480,7 @@ export default function Nav() {
         </form>
       </li>
     </ul>
-  )
+  );
 
   return (
     <nav className={styles.Nav} aria-label="We Build Black Site navigation">
@@ -495,5 +493,5 @@ export default function Nav() {
       </div>
       <div onKeyDown={handleMenuKeyPress}>{menuMarkup}</div>
     </nav>
-  )
+  );
 }
