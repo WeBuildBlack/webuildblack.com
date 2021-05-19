9faa0003ada31ee9614df761434020a823335f45
jackson- <jackson.t.devin@gmail.com>
2021-05-18 20:48:07 -0400

    fied url

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -316,8 +316,8 @@ export default function Nav() {
                 data-index={0}
                 tabIndex={-1}
                 ref={mavensRef}
-                data-href="www.mavens-io.com"
-                to="www.mavens-io.com"
+                data-href="/get-involved/events/mavens-conference"
+                to="https://mavens-io.com"
               >
                 Mavens I/O
               </Link>
