2e107a74e3d7074ff467c369121c26bde75216cd
jackson- <jackson.t.devin@gmail.com>
2021-10-01 02:45:50 -0400

    copy udpates

--- src/pages/fast-track.jsx
+++ src/pages/fast-track.jsx
@@ -6,6 +6,15 @@ import className from 'classnames'
 import { PageContainer } from '../components'
 import styles from '../assets/scss/fast-track.module.scss'
 
+import googleCerts from '../assets/images/google-certificates.png'
+import calendar from '../assets/images/fast-track-calendar.jpg'
+import microsoft from '../assets/images/microsoft.png'
+import shopify from '../assets/images/shopify.png'
+import google from '../assets/images/google-logo.png'
+import macbookIphone from '../assets/images/macbook-iphone.jpeg'
+import mentor from '../assets/images/Mentor1.jpg'
+import womanCoding from '../assets/images/woman-coding.jpg'
+
 export default function FastTrack({ data }) {
   
 
@@ -16,13 +25,13 @@ export default function FastTrack({ data }) {
         <div className={styles.HeroImage} />
         <section className={styles.IntroSection}>
           <div className={styles.SectionHeadingWrapper}>
-            <h1 className={styles.SectionHeading}>What is FAST TRACK</h1>
+            <h1 className={styles.SectionHeading}>What is FAST TRACK?</h1>
           </div>
 
           <div className={styles.IntroContentWrapper}>
             <div className={styles.IntroTextWrapper}>
               <p className={styles.IntroBody}>
-                {`“Every element of the program was in trouble and so were we. The simulators were not working, Mission Control was behind in virtually every area, and the flight and test procedures changed daily. Nothing we did had any shelf life. Not one of us stood up and said, ‘Dammit, stop!’ I don’t know what Thompson’s committee will find as the cause, but I know what I find. We are the cause! We were not ready! We did not do our job. We were rolling the dice, hoping that things would come together by launch day, when in our hearts we knew it would take a miracle. We were pushing the schedule and betting that the Cape would slip before we did.`}
+                {`FAST TRACK is the #1 tech workforce development program in the nation. We take Black women and those under the poverty line from techie-in-training to employed junior technologists in one year or less! That means education, dedicated mentors, resume assistance, and internships! Keep reading to learn more or click on one the button below to get involved.`}
               </p>
             </div>
           </div>
@@ -40,59 +49,82 @@ export default function FastTrack({ data }) {
             >
               Become a Student
             </button>
+            <button
+              type="button"
+              className={className('button special', styles.Button)}
+            >
+              Become a Mentor
+            </button>
           </div>
         </section>
 
         <section className={styles.Section}>
           <div className={styles.SectionHeadingWrapper}>
-            <h2 className={styles.SectionHeading}>Education</h2>
+            <h2 className={styles.SectionHeading}>Android Engineer, UX/UI Designer, Data Scientist! Which do you want to be?</h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src="https://images.unsplash.com/photo-1463171379579-3fdfb86d6285?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="Education" className={styles.SectionImage} />
+            <img src={googleCerts} alt="Education" className={styles.SectionImage} />
             <div className={styles.SectionText}>
-              <p>{`When you leave this meeting today you will go to your office and the first thing you will do there is to write ‘Tough and Competent’ on your blackboards. It will never be erased. Each day when you enter the room these words will remind you of the price paid by Grissom, White, and Chaffee. These words are the price of admission to the ranks of Mission Control.`}</p>
+              <p>{`Google has partnered with us to give our students scholarships to their online courses. They have training in Android Engineering, UX/UI Design, and Data Analysis. Google is so sure of their courses that they view a certificate from them as the equivalent to a college degree! So program participants can leave this course knowing they can shoot for a job at Google and be taken seriously.`}</p>
             </div>
           </div>
         </section>
 
         <section className={styles.Section}>
           <div className={styles.SectionHeadingWrapper}>
-            <h2 className={styles.SectionHeading}>Resources</h2>
+            <h2 className={styles.SectionHeading}>Free Macbook, iPhone, and learning stipends</h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src="https://images.unsplash.com/photo-1463171379579-3fdfb86d6285?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="Education" className={className(styles.SectionImage, 'image-right')} />
+            <img src={macbookIphone} alt="Education" className={className(styles.SectionImage, 'image-right')} />
             <div className={styles.SectionText}>
-              <p>{`We choose to go to the moon. We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win, and the others, too.`}</p>
-              <p>{`It is for these reasons that I regard the decision last year to shift our efforts in space from low to high gear as among the most important decisions that will be made during my incumbency in the office of the Presidency.`}</p>
+              <p>{`Our program is targeting Black women and those under the poverty line. That being said, we understand the financial barriers that come with committing time to learning something for free. In response to these challenges, our program is an earn-to-learn program where students will earn $1000 for every course milestone they achieve ($6000 total).`}</p>
+              <p>{`We’re also aware that not everyone has access to a computer or phone. To address this, every student in our program gets a new Macbook and iPhone.`}</p>
             </div>
           </div>
         </section>
 
         <section className={styles.Section}>
           <div className={styles.SectionHeadingWrapper}>
-            <h2 className={styles.SectionHeading}>Mentorships</h2>
+            <h2 className={styles.SectionHeading}>A mentor employed in your field of study</h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src="https://images.unsplash.com/photo-1463171379579-3fdfb86d6285?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="Education" className={styles.SectionImage} />
+            <img src={mentor} alt="Education" className={styles.SectionImage} />
             <div className={styles.SectionText}>
-              <p>{`“Every element of the program was in trouble and so were we. The simulators were not working, Mission Control was behind in virtually every area, and the flight and test procedures changed daily. Nothing we did had any shelf life. Not one of us stood up and said, ‘Dammit, stop!’ I don’t know what Thompson’s committee will find as the cause, but I know what I find. We are the cause! We were not ready! We did not do our job. We were rolling the dice, hoping that things would come together by launch day, when in our hearts we knew it would take a miracle. We were pushing the schedule and betting that the Cape would slip before we did.`}</p>
-              <p>{`“From this day forward, Flight Control will be known by two words: ‘Tough’ and ‘Competent.’ Tough means we are forever accountable for what we do or what we fail to do. We will never again compromise our responsibilities. Every time we walk into Mission Control we will know what we stand for. Competent means we will never take anything for granted. We will never be found short in our knowledge and in our skills. Mission Control will be perfect.`}</p>
+              <p>{`Data shows that only 5% of people finish their online courses. This is because when you get stuck, searching online can only take you so far. You need a real live person who’s been there and can hold your hand to the next step. Every program participant receives a dedicated mentor that is employed in their field of study at reputable tech companies.`}</p>
             </div>
           </div>
         </section>
 
         <section className={styles.Section}>
           <div className={styles.SectionHeadingWrapper}>
-            <h2 className={styles.SectionHeading}>Internships</h2>
+            <h2 className={styles.SectionHeading}>Full-time Internships</h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src="https://images.unsplash.com/photo-1463171379579-3fdfb86d6285?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="Education" className={className(styles.SectionImage, 'image-right')} />
+            <img src={womanCoding} alt="Education" className={className(styles.SectionImage, 'image-right')} />
             <div className={styles.SectionText}>
-              <p>{`The view of the earth from the moon fascinated me - a small disk, 240,000 mniles away. It was hard to think that that little thing held so many problems, so many frustrations. Raging nationalistic interests, famines, wars, pestilence don't show from that distance. I'm convinced that some wayward stranger in a space-craft, coming from some other part of the heavens, could look at earth and never know that it was inhabited at all. But the samw wayward stranger would certainly know instinctively that if the earth were inhabited, then the destinies of all who lived on it must inevitably be interwoven and joined. We are one hunk of ground, water, air, clouds, floating around in space. From out there it really is 'one world'.`}</p>
+              <p>{`A fast track into the industry isn’t complete without on-the-job training. We’ve partnered with Shopify to give internships to our students in data, design, and development. Their internships are full-time, well paid and as long as you perform well there’s a high chance you’ll be hired.`}</p>
+              <p>{`In the event that you don’t receive the job from Shopify, we’ve partnered with CareerCircle to give all of our students resume assistance and interview connections. CareerCircle’s whole business model is to get diverse people like you into interviews at top tech companies.`}</p>
             </div>
           </div>
         </section>
-
+        <section className={styles.Section}>
+          <div className={styles.SectionHeadingWrapper}>
+            <h2 className={styles.SectionHeading}>Program Calendar</h2>
+          </div>
+          <div className={styles.ButtonsContainer}>
+            <img src={calendar} alt="Education" className={className(styles.SectionImage)} />
+          </div>
+        </section>
+        <section className={styles.Section}>
+          <div className={styles.SectionHeadingWrapper}>
+            <h2 className={styles.SectionHeading}>Our Partners</h2>
+          </div>
+          <div className={styles.ButtonsContainer}>
+            <img src={shopify} alt="Education" className={className(styles.SectionImage)} />
+            <img src={google} alt="Education" className={className(styles.SectionImage)} />
+            <img src={microsoft} alt="Education" className={className(styles.SectionImage)} />
+          </div>
+        </section>
         <section className={styles.Section}>
           <div className={styles.ButtonsContainer}>
             <button
