fbf638a9d95c2dd5bb68bfcb08c2c4cf2fed6632
Brenton Cozby <5513633+BrentonCozby@users.noreply.github.com>
2021-10-01 09:52:31 -0400

    add animations

--- src/pages/fast-track.jsx
+++ src/pages/fast-track.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
-import Img from 'gatsby-image'
 import Helmet from 'react-helmet'
 import className from 'classnames'
 import { PageContainer } from '../components'
@@ -36,10 +34,11 @@ export default function FastTrack({ data }) {
             </div>
           </div>
 
-          <div className={styles.ButtonsContainer}>
+          <div className={styles.ButtonsContainer} data-sal="slide-up" data-sal-duration="1200" data-sal-easeing="ease-in-out">
             <button
               type="button"
               className={className('button special', styles.Button)}
+              
             >
               Become a Corporate Partner
             </button>
@@ -60,10 +59,19 @@ export default function FastTrack({ data }) {
 
         <section className={styles.Section}>
           <div className={styles.SectionHeadingWrapper}>
-            <h2 className={styles.SectionHeading}>Android Engineer, UX/UI Designer, Data Scientist! Which do you want to be?</h2>
+            <h2 className={styles.SectionHeading}>
+              Android Engineer, UX/UI Designer, Data Scientist! Which do you want to be?
+            </h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src={googleCerts} alt="Education" className={styles.SectionImage} />
+            <img
+              src={googleCerts}
+              alt="Google Certs"
+              className={styles.SectionImage}
+              data-sal="slide-up"
+              data-sal-duration="1200"
+              data-sal-easeing="ease-in-out"
+            />
             <div className={styles.SectionText}>
               <p>{`Google has partnered with us to give our students scholarships to their online courses. They have training in Android Engineering, UX/UI Design, and Data Analysis. Google is so sure of their courses that they view a certificate from them as the equivalent to a college degree! So program participants can leave this course knowing they can shoot for a job at Google and be taken seriously.`}</p>
             </div>
@@ -75,7 +83,14 @@ export default function FastTrack({ data }) {
             <h2 className={styles.SectionHeading}>Free Macbook, iPhone, and learning stipends</h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src={macbookIphone} alt="Education" className={className(styles.SectionImage, 'image-right')} />
+            <img
+              src={macbookIphone}
+              alt="MacBook iPhone"
+              className={className(styles.SectionImage, 'image-right')}
+              data-sal="slide-up"
+              data-sal-duration="1200"
+              data-sal-easeing="ease-in-out"
+            />
             <div className={styles.SectionText}>
               <p>{`Our program is targeting Black women and those under the poverty line. That being said, we understand the financial barriers that come with committing time to learning something for free. In response to these challenges, our program is an earn-to-learn program where students will earn $1000 for every course milestone they achieve ($6000 total).`}</p>
               <p>{`We’re also aware that not everyone has access to a computer or phone. To address this, every student in our program gets a new Macbook and iPhone.`}</p>
@@ -88,7 +103,14 @@ export default function FastTrack({ data }) {
             <h2 className={styles.SectionHeading}>A mentor employed in your field of study</h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src={mentor} alt="Education" className={styles.SectionImage} />
+            <img
+              src={mentor}
+              alt="Mentor"
+              className={styles.SectionImage}
+              data-sal="slide-up"
+              data-sal-duration="1200"
+              data-sal-easeing="ease-in-out"
+            />
             <div className={styles.SectionText}>
               <p>{`Data shows that only 5% of people finish their online courses. This is because when you get stuck, searching online can only take you so far. You need a real live person who’s been there and can hold your hand to the next step. Every program participant receives a dedicated mentor that is employed in their field of study at reputable tech companies.`}</p>
             </div>
@@ -100,7 +122,14 @@ export default function FastTrack({ data }) {
             <h2 className={styles.SectionHeading}>Full-time Internships</h2>
           </div>
           <div className={styles.SectionContent}>
-            <img src={womanCoding} alt="Education" className={className(styles.SectionImage, 'image-right')} />
+            <img
+              src={womanCoding}
+              alt="Woman Coding"
+              className={className(styles.SectionImage, 'image-right')}
+              data-sal="slide-up"
+              data-sal-duration="1200"
+              data-sal-easeing="ease-in-out"
+            />
             <div className={styles.SectionText}>
               <p>{`A fast track into the industry isn’t complete without on-the-job training. We’ve partnered with Shopify to give internships to our students in data, design, and development. Their internships are full-time, well paid and as long as you perform well there’s a high chance you’ll be hired.`}</p>
               <p>{`In the event that you don’t receive the job from Shopify, we’ve partnered with CareerCircle to give all of our students resume assistance and interview connections. CareerCircle’s whole business model is to get diverse people like you into interviews at top tech companies.`}</p>
@@ -112,7 +141,7 @@ export default function FastTrack({ data }) {
             <h2 className={styles.SectionHeading}>Program Calendar</h2>
           </div>
           <div className={styles.ButtonsContainer}>
-            <img src={calendar} alt="Education" className={className(styles.SectionImage)} />
+            <img src={calendar} alt="Calendar" className={className(styles.SectionImage)} />
           </div>
         </section>
         <section className={styles.Section}>
@@ -120,9 +149,9 @@ export default function FastTrack({ data }) {
             <h2 className={styles.SectionHeading}>Our Partners</h2>
           </div>
           <div className={styles.ButtonsContainer}>
-            <img src={shopify} alt="Education" className={className(styles.SectionImage)} />
-            <img src={google} alt="Education" className={className(styles.SectionImage)} />
-            <img src={microsoft} alt="Education" className={className(styles.SectionImage)} />
+            <img src={shopify} alt="Shopfiy" className={className(styles.SectionImage)} />
+            <img src={google} alt="Google" className={className(styles.SectionImage)} />
+            <img src={microsoft} alt="Microsoft" className={className(styles.SectionImage)} />
           </div>
         </section>
         <section className={styles.Section}>
@@ -145,29 +174,3 @@ export default function FastTrack({ data }) {
     </PageContainer>
   )
 }
-
-// export const query = graphql`
-//   query FastTrack {
-//     allMarkdownRemark(filter: { frontmatter: { page: { eq: "about" } } }) {
-//       nodes {
-//         frontmatter {
-//           teamMembers {
-//             bio
-//             name
-//             role
-//             imageName
-//           }
-//         }
-//       }
-//     }
-//     allFile(filter: { relativeDirectory: { eq: "about" } }) {
-//       nodes {
-//         childImageSharp {
-//           fluid {
-//             ...GatsbyImageSharpFluid
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
